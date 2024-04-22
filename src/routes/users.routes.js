1e101430c77ce9bf737b2826cdf573f368800854
Warlley Lopes <warlleylopes123@gmail.com>
2024-04-22 23:31:08 +0100

    image upload development

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,24 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UserController = require("../controllers/UsersController");
-const ensuredAuthenticated = require("../middlewares/ensuredAuthenticated")
+const UserAvatarController = require("../controllers/UserAvatarController");
+const ensuredAuthenticated = require("../middlewares/ensuredAuthenticated");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
-const usersController = new UserController()
+const usersController = new UserController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
-usersRoutes.put("/", ensuredAuthenticated, usersController.update)
+usersRoutes.put("/", ensuredAuthenticated, usersController.update);
+usersRoutes.patch(
+  "/avatar",
+  ensuredAuthenticated,
+  upload.single("avatar"),
+  userAvatarController.update
+);
 
 module.exports = usersRoutes;
